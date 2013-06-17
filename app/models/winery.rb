9d924759f4f99a81dd992e2075573b2a596fe04d
Sean Doe-Simkins <sdoesimkins@gmail.com>
2013-06-17 12:36:45 -0500

    Added destroy dependent records

--- app/models/winery.rb
+++ app/models/winery.rb
@@ -1,7 +1,7 @@
 class Winery < ActiveRecord::Base
   attr_accessible :location, :name
   validates :name, :presence => true
-  has_many :wines
+  has_many :wines, :dependent => :destroy
   extend FriendlyId
   friendly_id :name, use: [:slugged, :history]
 end
